7069108896b99be795dbe071fb717434aedcec93
Kareamahmed <kareama47@gmail.com>
2026-08-26 07:24:18 +0300

    Add created_at and updated_at fields to DataChunk model and define RetrievedDocument class

--- src/models/db_schemes/arabic_legal/schemes/DataChunk.py
+++ src/models/db_schemes/arabic_legal/schemes/DataChunk.py
@@ -1,5 +1,6 @@
 from .arabic_legal_base import SQLAlchemyBase
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, func, DateTime
+from pydantic import BaseModel
 
 
 class DataChunk(SQLAlchemyBase):
@@ -8,3 +9,14 @@ class DataChunk(SQLAlchemyBase):
     chunk_id = Column(Integer, primary_key=True, autoincrement=True)
     chunk_text = Column(String, nullable=False)
     chunk_metadata = Column(String, nullable=True)
+
+    created_at = Column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=True)
+
+
+class RetrievedDocument(BaseModel):
+    chunk_id: int
+    chunk_text: str
+    score: float
